44bc04c538b37281a8bd285c999cf9337edf13be
va3dxv <va3dxv@CALLOUS.disregard.net>
2018-12-11 09:34:12 -0500

    menu items

--- allmon.ini.php
+++ allmon.ini.php
@@ -1,11 +1,28 @@
 <?php
-[Off-site Link]
-url=http://allstarlink.org
 
 [49078]
-host=127.0.0.1:5038
+host=127.0.0.1:5033
 user=admin
-passwd=CHANGEME
-nomenu=no
+passwd=saumanagement123
+nomenu=yes
 hideNodeURL=no
 
+[AllStarLink Portal]
+url=https://web-tpa.allstarlink.org/
+
+[AllStarLink Documentation]
+url=http://docs.allstarlink.org
+
+[Online Allstar Nodes]
+url=http://stats.allstarlink.org/
+
+[Brandmeister Hotspot Management]
+url=https://brandmeister.network/
+
+[Listen to DMR TG 302613]
+url=http://hose.brandmeister.network/group/302613/
+
+[DMR Radio ID Database]
+url=http://www.radioid.net/
+
+?>
