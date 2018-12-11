a1f364ecc0f016577d31dacf11194ac1c58f7269
unfknreal <unfknreal@gmail.com>
2018-12-11 09:24:11 -0500

    Update allmon.ini.php

--- allmon.ini.php
+++ allmon.ini.php
@@ -5,24 +5,7 @@ url=http://allstarlink.org
 [49078]
 host=127.0.0.1:5038
 user=admin
-passwd=saumanagement123
+passwd=CHANGEME
 nomenu=no
 hideNodeURL=no
 
-;[2000]
-;system=System2
-;host=my.address.com:5037
-;user=admin
-;passwd=password2
-
-;[2000 RTCM]
-;system=System2
-;rtcmnode=2000
-
-;[Grouped Nodes]
-;nodes=500,501,2000
-
-;[2 RTCMs]
-;system=System2
-;rtcmnode=2000,2001
-?>
